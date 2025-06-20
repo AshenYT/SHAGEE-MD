f0d75ea4451ad68d7b78573dd0818c55357e997c
bro-panel <imdbro7@gmail.com>
2025-06-20 20:25:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Y60hRCzC#iV6w4Sbo-VtRLSYMcBP4VDtrkwS7-xUzDXeIEbEAByk",
+SESSION_ID: process.env.SESSION_ID || "HEQ2WITL#zcSqGoJTsZGF4rS40DBwXa_ING4ejEYjBVKtxUNnTwU",
 MONGODB: process.env.MONGODB || "mongodb://mongo:pyytHDPIvoywGRugWvKeOorMaulrSrTx@yamabiko.proxy.rlwy.net:28068", //Railway or atlas eg: mongodb+srv://example:mongo@cluster0.7kjia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/39kWRqJs/320.jpg",
 MODE :process.env.MODE || "public"  

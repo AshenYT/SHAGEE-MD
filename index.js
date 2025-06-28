ae73f2bd5db1ee4918acc4c0b4287fa1ba92eb25
bro-panel <imdbro7@gmail.com>
2025-06-28 22:48:41 +0530

    𝐒𝙷𝙰𝙶𝙴𝙴 𝐌𝙳 ██▌▍▌▍██

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (!config.SESSION_ID) {
   return console.log('Please add your session to SESSION_ID env !!');
 }
 
-const sessdata = config.SESSION_ID.replace('SHAGEE=', '');
+const sessdata = config.SESSION_ID.replace('3KBOT', '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`);
 
 filer.download((err, data) => {
